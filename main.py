c9212d856f74d4be826eb1c0ea5c674aabb57a47
JPRovezzi <100390644+JPRovezzi@users.noreply.github.com>
2024-11-05 14:32:38 -0300

    Welcome Screen

--- main.py
+++ main.py
@@ -1,27 +1,108 @@
+# Description: This script is the main script for the UGROpyGUI application. 
+# It takes a molecule name as input and displays the molecule groups in a GUI window.
+
+# Import the required libraries
+# UgroPy is a Python library that provides a simple interface to the UNIFAC group contribution method.
 from ugropy import Groups
 from ugropy import unifac
+# Tkinter is a standard GUI library for Python.
 import tkinter as tk
 from tkinter import simpledialog
-from rdkit import Chem
-from ugropy import DefaultSolver
-from rdkit.Chem import Draw
-from IPython.display import SVG
+# The subprocess library is used to run external commands.
 import subprocess
+# The os library provides a way to interact with the operating system.
 import os
+# The PIL library is used to work with images.
 from PIL import Image, ImageDraw, ImageFont
 
 #------------------------------------------------------------
+# Functions to display the GUI windows
 
-def get_molecule_name():
+def window_welcome():
+    '''Display a welcome window.'''
+    window = tk.Tk()
+    window.title(title)
+    label = tk.Label(window, text="\nWelcome to UGROpyGUI!\n")
+    label.pack()
+    start_button = tk.Button(window, text="Start", command=window.destroy)
+    start_button.pack()
+    
+    set_window_size(window)
+    center_window(window)
+    window.mainloop()
+
+def window_selection():
+    '''Display a window to select the input type (name or SMILES code).'''
+    window = tk.Tk()
+    window.title(title)
+    label = tk.Label(window, text="\nSelect the input type:\n")
+    label.pack()
+    global input_type
+    input_type = None
+    def select_name():
+        window.destroy()
+        global input_type
+        input_type = 'name'
+    
+    def select_smiles():
+        window.destroy()
+        global input_type
+        input_type = 'smiles'
+
+    def select_exit():
+        window.destroy()
+        global input_type
+        input_type = 'exit'
+    
+    name_button = tk.Button(window, text="Name", command=select_name)
+    name_button.pack()
+    
+    smiles_button = tk.Button(window, text="SMILES", command=select_smiles)
+    smiles_button.pack()
+    label = tk.Label(window, text="\n")
+    label.pack()
+    smiles_button = tk.Button(window, text="Exit", command=select_exit)
+    smiles_button.pack()
+    
+    
+    set_window_size(window)
+    center_window(window)
+    window.mainloop()
+    print(input_type)
+    return input_type
+
+def window_get_name_old(text):
+    '''Display a window to get the molecule name from the user.'''
     root = tk.Tk()
     root.title(title)
     root.withdraw()  # Hide the main window
-    molecule_name = simpledialog.askstring("Input", "Please enter the molecule name:")
+    molecule_name = simpledialog.askstring("Input", "Please enter the molecule "+text+" :")
+    
+    set_window_size(root)
     center_window(root)
     root.destroy()
     return molecule_name
+def window_get_name(text):
+    '''Display a window to get the molecule name from the user.'''
+    window = tk.Tk()
+    window.title(title)
+    label = tk.Label(window, text="Please enter the molecule " + text + " :")
+    label.pack(pady=10)
+    molecule_name_var = tk.StringVar()
+    entry = tk.Entry(window, textvariable=molecule_name_var)
+    entry.pack(pady=10)
+    def submit():
+        window.destroy()
+    submit_button = tk.Button(window, text="Submit", command=submit)
+    submit_button.pack(pady=10)
+    
+    set_window_size(window)
+    center_window(window)
+    window.mainloop()
+    return molecule_name_var.get()
 
 def window_message(message):
+    '''Display a window with a message.'''
     window = tk.Tk()
     window.title(title)
     label = tk.Label(window, text="\n"+message+"\n")
@@ -31,10 +112,13 @@ def window_message(message):
     label.pack()
     close_button = tk.Button(window, text="Close", command=window.destroy)
     close_button.pack()
+    
+    set_window_size(window)
     center_window(window)
     window.mainloop()
 
 def window_picture(message):
+    '''Display a window with a message and an image.'''
     window = tk.Tk()
     window.title(title)
     label = tk.Label(window, text="\n"+message+"\n")
@@ -46,10 +130,13 @@ def window_picture(message):
     label.pack()
     close_button = tk.Button(window, text="Close", command=window.destroy)
     close_button.pack()
+    
+    set_window_size(window)
     center_window(window)
     window.mainloop()
 
 def center_window(window):
+    '''Center the window on the screen.'''
     window.update_idletasks()
     width = window.winfo_width()
     height = window.winfo_height()
@@ -58,7 +145,13 @@ def center_window(window):
     window.geometry(f'{width}x{height}+{x}+{y}')
     return None
 
+def set_window_size(window, width=640, height=480):
+    '''Set the window size to 640x480 pixels by default.'''
+    window.geometry(f'{width}x{height}')
+    return None
+
 def run_c_script(c_script_path: str, output_path: str):
+    '''Compile and run a C script.'''
     if not os.path.exists("SvgToPng.exe"):  
         # Compile the C script
         compile_command = f"gcc {c_script_path} -o {output_path}"
@@ -69,7 +162,8 @@ def run_c_script(c_script_path: str, output_path: str):
     run_process = subprocess.run(run_command)
     return run_process.stdout
 
-def writeInPicture(dict):
+def write_groups2picture(dict):
+    '''Write the molecule groups in the picture.'''
     # Load the image
     image = Image.open('output.png')
     print("picture loaded")
@@ -94,32 +188,33 @@ def writeInPicture(dict):
     return None
 
 #------------------------------------------------------------
-title = "UGROpyGUI"
+title = "UGROpyGUI" # Set the title of the GUI window
 
+
+window_welcome() # Display the welcome window    
 while True:
-    molecule_name = get_molecule_name()
+    input_type = window_selection()
+    if input_type != 'exit' and input_type != None:
+        molecule_name = window_get_name(input_type) # Get the molecule name from the user
+        if molecule_name:
+            try:
+                molecule = Groups(identifier=molecule_name,identifier_type=input_type)
+                molecule_groups = unifac.get_groups(identifier=molecule_name,identifier_type=input_type)
+            except:
+                window_message("Invalid molecule name.")
+                continue
+            svg_string = molecule_groups.get_solution_svg() # Get the SVG information
+            with open("input.svg", "w") as file:
+                file.write(svg_string) #Save the SVG
+            run_c_script("SvgToPng.c", "SvgToPng") # Run the C script to convert the SVG to PNG
+            write_groups2picture(molecule.unifac.subgroups) # Write the molecule groups in the picture
+            window_picture("The molecule groups are displayed below.")
 
-    if molecule_name:
-        try:
-            molecule = Groups(molecule_name)
-            molecule_groups = unifac.get_groups(molecule_name)
-        except:
-            print("Invalid molecule name.")
-            window_message("Invalid molecule name.")
-            continue
-        svg_string = molecule_groups.get_solution_svg()
-        with open("input.svg", "w") as file:
-            file.write(svg_string)
-        
-        run_c_script("SvgToPng.c", "SvgToPng")
-        writeInPicture(molecule.unifac.subgroups)
-        window_picture("The molecule groups are displayed below.")
-        print(molecule.unifac.subgroups)
-
-
+        else:
+            print("No molecule name provided.")
+            continue # Exit the loop if no molecule name is provided
     else:
-        print("No molecule name provided.")
-        exit()
+        break # Exit the loop if no input type is selected
 
 
 
