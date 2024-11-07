9fd0269db96dac7118c972c46d3ce419ed67577d
JPRovezzi <100390644+JPRovezzi@users.noreply.github.com>
2024-11-07 11:32:54 -0300

    New GUI

--- main.py
+++ main.py
@@ -7,148 +7,17 @@ from ugropy import Groups
 from ugropy import unifac
 # Tkinter is a standard GUI library for Python.
 import tkinter as tk
-from tkinter import simpledialog
+
+#from tkinter import simpledialog
 # The subprocess library is used to run external commands.
 import subprocess
 # The os library provides a way to interact with the operating system.
 import os
 # The PIL library is used to work with images.
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageTk
 
 #------------------------------------------------------------
 # Functions to display the GUI windows
-
-def window_welcome():
-    '''Display a welcome window.'''
-    window = tk.Tk()
-    window.title(title)
-    label = tk.Label(window, text="\nWelcome to UGROpyGUI!\n")
-    label.pack()
-    start_button = tk.Button(window, text="Start", command=window.destroy)
-    start_button.pack()
-    
-    set_window_size(window)
-    center_window(window)
-    window.mainloop()
-
-def window_selection():
-    '''Display a window to select the input type (name or SMILES code).'''
-    window = tk.Tk()
-    window.title(title)
-    label = tk.Label(window, text="\nSelect the input type:\n")
-    label.pack()
-    global input_type
-    input_type = None
-    def select_name():
-        window.destroy()
-        global input_type
-        input_type = 'name'
-    
-    def select_smiles():
-        window.destroy()
-        global input_type
-        input_type = 'smiles'
-
-    def select_exit():
-        window.destroy()
-        global input_type
-        input_type = 'exit'
-    
-    name_button = tk.Button(window, text="Name", command=select_name)
-    name_button.pack()
-    
-    smiles_button = tk.Button(window, text="SMILES", command=select_smiles)
-    smiles_button.pack()
-    label = tk.Label(window, text="\n")
-    label.pack()
-    smiles_button = tk.Button(window, text="Exit", command=select_exit)
-    smiles_button.pack()
-    
-    
-    set_window_size(window)
-    center_window(window)
-    window.mainloop()
-    print(input_type)
-    return input_type
-
-def window_get_name_old(text):
-    '''Display a window to get the molecule name from the user.'''
-    root = tk.Tk()
-    root.title(title)
-    root.withdraw()  # Hide the main window
-    molecule_name = simpledialog.askstring("Input", "Please enter the molecule "+text+" :")
-    
-    set_window_size(root)
-    center_window(root)
-    root.destroy()
-    return molecule_name
-def window_get_name(text):
-    '''Display a window to get the molecule name from the user.'''
-    window = tk.Tk()
-    window.title(title)
-    label = tk.Label(window, text="Please enter the molecule " + text + " :")
-    label.pack(pady=10)
-    molecule_name_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=molecule_name_var)
-    entry.pack(pady=10)
-    def submit():
-        window.destroy()
-    submit_button = tk.Button(window, text="Submit", command=submit)
-    submit_button.pack(pady=10)
-    
-    set_window_size(window)
-    center_window(window)
-    window.mainloop()
-    return molecule_name_var.get()
-
-def window_message(message):
-    '''Display a window with a message.'''
-    window = tk.Tk()
-    window.title(title)
-    label = tk.Label(window, text="\n"+message+"\n")
-    #image = tk.PhotoImage(file="./output.png")
-    #image_label = tk.Label(window, image=image)
-    #image_label.pack()
-    label.pack()
-    close_button = tk.Button(window, text="Close", command=window.destroy)
-    close_button.pack()
-    
-    set_window_size(window)
-    center_window(window)
-    window.mainloop()
-
-def window_picture(message):
-    '''Display a window with a message and an image.'''
-    window = tk.Tk()
-    window.title(title)
-    label = tk.Label(window, text="\n"+message+"\n")
-    label.pack()
-    label = tk.Label(window, text=molecule.unifac.subgroups)
-    image = tk.PhotoImage(file="./output.png")
-    image_label = tk.Label(window, image=image)
-    image_label.pack()
-    label.pack()
-    close_button = tk.Button(window, text="Close", command=window.destroy)
-    close_button.pack()
-    
-    set_window_size(window)
-    center_window(window)
-    window.mainloop()
-
-def center_window(window):
-    '''Center the window on the screen.'''
-    window.update_idletasks()
-    width = window.winfo_width()
-    height = window.winfo_height()
-    x = (window.winfo_screenwidth() // 2) - (width // 2)
-    y = (window.winfo_screenheight() // 2) - (height // 2)
-    window.geometry(f'{width}x{height}+{x}+{y}')
-    return None
-
-def set_window_size(window, width=640, height=480):
-    '''Set the window size to 640x480 pixels by default.'''
-    window.geometry(f'{width}x{height}')
-    return None
 
 def run_c_script(c_script_path: str, output_path: str):
     '''Compile and run a C script.'''
@@ -166,7 +35,7 @@ def write_groups2picture(dict):
     '''Write the molecule groups in the picture.'''
     # Load the image
     image = Image.open('output.png')
-    print("picture loaded")
+    #print("picture loaded")
     # Initialize ImageDraw
     draw = ImageDraw.Draw(image)
 
@@ -187,34 +56,273 @@ def write_groups2picture(dict):
     image.save('output.png')
     return None
 
-#------------------------------------------------------------
-title = "UGROpyGUI" # Set the title of the GUI window
+def load_frame_welcome():
+    clear_widgets_except(frame_welcome)
+    frame_welcome.tkraise()
+    frame_welcome.pack_propagate(False)
+    # frame_welcome widgets
+    tk.Label(
+        frame_welcome, 
+        text="\nWelcome to UGROpyGUI!\n",
+        bg=bg_color,
+        fg="white",
+        font=("TkMenuFont",14)
+        ).pack()
+
+    tk.Button(
+        frame_welcome,
+        text="START",
+        fg="black",
+        font=("TkMenuFont",12),
+        bg="white",
+        cursor="hand2",
+        activebackground="gray",
+        command=lambda:load_frame_selection()
+        ).pack(pady=10)
+    
+    tk.Button(
+    frame_welcome,
+    text="EXIT",
+    fg="black",
+    font=("TkMenuFont",12),
+    bg="white",
+    cursor="hand2",
+    activebackground="gray",
+    command=lambda:root.destroy()
+    ).pack(pady=10)
+
+    return None
+
+def load_frame_selection(error_message=None):
+    def select_name():
+        #global input_type
+        input_type = 'name'
+        molecule_id = molecule_id_var.get()
+        outcome = get_results(molecule_id,input_type)
+        molecule,error = outcome
+        if error == None and molecule != None:
+            load_frame_result(molecule)
+            error_message = ""
+        elif error == 1:
+            clear_widgets_except(None)
+            error_message = "The NAME identifier is not valid. Please try again."
+            load_frame_selection(error_message)
+        elif error == 2:
+            clear_widgets_except(None)
+            error_message = "You must enter a NAME identifier. Please try again."
+            load_frame_selection(error_message)
+        else: 
+            error_message = "Unknown error. Please try again."
+            clear_widgets_except(None)
+            load_frame_selection(error_message)
+
+    def select_smiles():
+        #global input_type
+        input_type = 'smiles'
+        
+        molecule_id = molecule_id_var.get()
+        outcome = get_results(molecule_id,input_type)
+        molecule,error = outcome
+        if error == None and molecule != None:
+            load_frame_result(molecule)
+            error_message = ""
+        elif error == 1:
+            clear_widgets_except(None)
+            error_message = "The SMILES identifier is not valid. Please try again."
+            load_frame_selection(error_message)
+        elif error == 2:
+            clear_widgets_except(None)
+            error_message = "You must enter a SMILES identifier. Please try again."
+            load_frame_selection(error_message)
+        else: 
+            error_message = "Unknown error. Please try again."
+            clear_widgets_except(None)
+            load_frame_selection(error_message)
+
+    clear_widgets_except(frame_selection)
+    frame_selection.tkraise()
+
+    # frame_selection widgets
+    label = tk.Label(frame_selection,text="",bg=bg_color).pack(pady=0)
+    label = tk.Label(
+        frame_selection, 
+        text = "Please enter the Chemical identifier of the molecule:",
+        bg=bg_color,
+        fg="white",
+        font=("TkMenuFont",14)
+        ).pack(pady=0)
+    molecule_id_var = tk.StringVar()
+    entry = tk.Entry(
+        frame_selection, 
+        textvariable = molecule_id_var
+        ).pack(pady=20)
+
+    label = tk.Label(
+        frame_selection, 
+        text = error_message,
+        bg=bg_color,
+        fg="red",
+        font=(14)
+        ).pack(pady=0)
+
+    tk.Label(
+        frame_selection, 
+        text="Select the type of Chemical identifier",
+        bg=bg_color,
+        fg="white",
+        font=("TkMenuFont",14)
+        ).pack()
+
+    tk.Button(
+        frame_selection,
+        text="NAME",
+        fg="black",
+        font=("TkMenuFont",12),
+        bg="white",
+        cursor="hand2",
+        activebackground="gray",
+        command=lambda:select_name()
+        ).pack(pady=10)
+
+    tk.Button(
+        frame_selection,
+        text="SMILES",
+        fg="black",
+        font=("TkMenuFont",12),
+        bg="white",
+        cursor="hand2",
+        activebackground="gray",
+        command=lambda:select_smiles()
+        ).pack(pady=10)
+
+    tk.Button(
+        frame_selection,
+        text="BACK",
+        fg="black",
+        font=("TkMenuFont",12),
+        bg="white",
+        cursor="hand2",
+        activebackground="gray",
+        command=lambda:load_frame_welcome()
+        ).pack(pady=50)
+
+    return None
+
+def load_frame_result(molecule):
+    
+    clear_widgets_except(frame_result)
+    frame_result.tkraise()
+    
+    tk.Label(
+        frame_result,
+        text="The molecule groups are displayed below.",
+        bg=bg_color,
+        fg="white",
+        font=("TkMenuFont",14)
+        ).pack(pady=20)
+    
+    image=ImageTk.PhotoImage(file="./output.png")
+    image_widget = tk.Label(
+        frame_result,
+        image = image
+        )
+    image_widget.image = image
+    image_widget.pack()
+
+    tk.Label(
+        frame_result,
+        text = molecule.unifac.subgroups,
+        bg=bg_color,
+        fg="white",
+        ).pack()
+
+    tk.Button(
+        frame_result,
+        text="BACK",
+        fg="black",
+        font=("TkMenuFont",12),
+        bg="white",
+        cursor="hand2",
+        activebackground="gray",
+        command=lambda:load_frame_welcome()
+        ).pack(pady=50)
+    
+    return None
 
 
-window_welcome() # Display the welcome window    
-while True:
-    input_type = window_selection()
-    if input_type != 'exit' and input_type != None:
-        molecule_name = window_get_name(input_type) # Get the molecule name from the user
-        if molecule_name:
+def clear_widgets_except(currentFrame):
+    for frame in frames:
+        if frame != currentFrame:
+            for widget in frame.winfo_children():
+                widget.destroy()
+    return None
+
+def get_results(molecule_id,input_type):  
+    outcome = (None,None)
+    if molecule_id != None and molecule_id != "":
             try:
-                molecule = Groups(identifier=molecule_name,identifier_type=input_type)
-                molecule_groups = unifac.get_groups(identifier=molecule_name,identifier_type=input_type)
+                #print(input_type, molecule_id)
+                molecule = Groups(
+                    identifier = molecule_id,
+                    identifier_type = input_type
+                    )
+                molecule_groups = unifac.get_groups(
+                    identifier = molecule_id,
+                    identifier_type = input_type
+                    )
             except:
-                window_message("Invalid molecule name.")
-                continue
+                error = 1
+                #print(1)
+                outcome = (None, error)
+                return outcome
             svg_string = molecule_groups.get_solution_svg() # Get the SVG information
             with open("input.svg", "w") as file:
                 file.write(svg_string) #Save the SVG
             run_c_script("SvgToPng.c", "SvgToPng") # Run the C script to convert the SVG to PNG
             write_groups2picture(molecule.unifac.subgroups) # Write the molecule groups in the picture
-            window_picture("The molecule groups are displayed below.")
+            #print("The molecule groups are displayed below.")
+            error = None
+            #print(0)
+            outcome = (molecule, error)
+            return outcome
 
-        else:
-            print("No molecule name provided.")
-            continue # Exit the loop if no molecule name is provided
     else:
-        break # Exit the loop if no input type is selected
+        error = 2
+        #print(2)
+        outcome = (None,error)
+        return outcome
+    
+#------------------------------------------------------------
+
+# Basic configuration
+bg_color = '#f36c31' #IPQA Color
+
+# Global variables
+molecule_id = "" # Variable to store the molecule identifier
+
+# Create the main window
+root = tk.Tk()
+root.title("UGROpyGUI")
+root.resizable(0,0) # Disable resizing
+root.eval("tk::PlaceWindow . center")
+
+# Create a frame widget
+frame_welcome = tk.Frame(root, width=640, height=480, bg=bg_color)
+frame_selection = tk.Frame(root, width=640, height=480, bg=bg_color)
+frame_getName = tk.Frame(root, width=640, height=480, bg=bg_color)
+frame_result = tk.Frame(root, width=640, height=480, bg=bg_color)
+frames = (frame_welcome,frame_selection, frame_getName, frame_result)
+
+for frame in frames:
+    frame.grid_rowconfigure(0, weight=1)
+    frame.grid_columnconfigure(0, weight=1)
+    frame.grid(row=0, column=0, sticky="nesw")
+
+load_frame_welcome()
+
+root.mainloop()
+
+
 
 
 
